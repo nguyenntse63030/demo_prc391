a783f50c2296e6841beba14c651bd869e6a2d12a
nguyenntse63030 <41613848+nguyenntse63030@users.noreply.github.com>
2020-03-07 15:43:34 +0700

    Delete config.js

--- configs.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    AWS: {
-        ACCESS_KEY_ID: 'AKIAJUQVQAPCOPSFBXJQ',
-        SECRETT_KEY: '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',
-        BUCKET_NAME: 'prc391-bucket'
-    }
-}
